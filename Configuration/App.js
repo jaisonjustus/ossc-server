29ebe9a88e6a950c5ad60728330dcca0b912b2d6
jaisonjustus <jaison.justus.lp@gmail.com>
2012-12-10 16:12:10 +0530

    adding ossc-server base files

---
+++ Configuration/App.js
@@ -0,0 +1,9 @@
+module.exports = {
+	server : {
+		port : 8080
+	},
+	mongo : {
+		connectionString : 'mongodb://<username>:<password>@<host>:<port>/<db-name>',
+		dbName : 'collectionexchange'
+	}
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+/** Defining module varaibles; **/
+var restify, mongoose, underscore, config, route;
+
+/** Defining variables; **/
+var server, db, schema;
+
+/**
+ * Including dependencies
+*/
+restify = require('restify');
+mongoose = require('mongoose');
+underscore = require('underscore');
+config = require('./Configuration/App');
+
+/**
+ * Preparing Application server;
+ */
+server = restify.createServer();
+server.use(restify.bodyParser());
+
+/**
+ * Preparing and connecting to Mongo Database;
+ */
+mongoose.connect(config.mongo.connectionString);
+
+/**
+ * Binding dependency modules for other tiers
+ */
+module.exports.app = {
+	mongoose : mongoose,
+	server : server,
+	underscore : underscore
+};
+
+/**
+ * Loading the route file and initializing it.
+ */
+route = require('./Configuration/Route');
+route.open();
+
+/** Server listening at port 8080 **/
+server.listen(config.server.port);

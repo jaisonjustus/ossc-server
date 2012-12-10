6b3b6c9fce53fda1090dc3beb7f598e086109af4
jaisonjustus <jaison.justus.lp@gmail.com>
2012-12-10 16:28:07 +0530

    during the last commit the file not uploaded. updating the configuration file for redis configuration and added comments on it;

--- Configuration/App.js
+++ Configuration/App.js
@@ -1,9 +1,24 @@
+/**
+ * Application Configuarion File;
+ */
 module.exports = {
+	/** Application server configuration; **/
 	server : {
 		port : 8080
 	},
+	
+	/** MongoDB configuration; **/
 	mongo : {
 		connectionString : 'mongodb://<username>:<password>@<host>:<port>/<db-name>',
 		dbName : 'collectionexchange'
+	},
+
+	/** Redis configuration; **/
+	redis : {
+		connectionString : "",
+		server : "",
+		port : "",
+		instance : ""
 	}
+
 };
